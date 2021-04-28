3e0351721dda24c6ad4f62f9219badad88a0a2aa
Waqas <wakassadik@gmail.com>
2021-04-28 23:24:47 +0500

    adding second recipe in list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,10 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/15latkes.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/15latkes.jpg'),
+    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/15latkes.jpg')
+
+  
   ];
   constructor() { }
 
